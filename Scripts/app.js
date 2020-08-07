9f8bdaa676a87c6096569c1cecec76799439d78c
Simran145678 <harsimrankaur708@gmail.com>
2020-08-06 23:47:26 -0400

    contact page created

--- Scripts/app.js
+++ Scripts/app.js
@@ -15,7 +15,31 @@
       console.log(SubmitButton);
       }
  */
+    /* let title=document.title;
+    console.log(`The title of the page is ${title}`) */
+    let title=document.title;
+    title=title.toLowerCase();
 
+    let navAnchors = document.querySelectorAll("li a");
+  
+    for (const anchor of navAnchors) {
+
+      let anchorString=anchor.getAttribute("href");
+     anchorString=anchorString.substr(0,anchorString.length-5);
+     if((title==="home")&&(anchorString==="index")||(title === anchorString))
+      {
+        anchor.className="nav-link active";
+      }
+      console.log(anchorString);
+    }
+
+    let indexParagraph=document.getElementsByClassName("Paragraph");
+
+    let paraIndex=document.createElement("p");
+
+    paraIndex.textContent="This is the index paragraph";
+
+    Paragraph.appendchild(paraIndex);
 
     }
 
